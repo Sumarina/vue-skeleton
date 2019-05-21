b33ee983875702a713172b4568a33ae2379ed199
sumarina <marinzoo@hotmail.com>
2019-05-21 23:53:18 +0800

    解决服务端渲染构建异常问题

--- webpack.skeleton.js
+++ webpack.skeleton.js
@@ -19,13 +19,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ]
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader'
       }
